d6d24b4a7b5fc932260a5ae3fc171efe5b13b37f
Lin Lập Kiến <linlapkien@gmail.com>
2024-08-20 11:46:26 +0700

    Host App Remote Component App and Pass Props as Data fetching from API

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,57 @@
+import NextFederationPlugin from '@module-federation/nextjs-mf';
+
+const remotes = (isServer) => {
+  const location = isServer ? 'ssr' : 'chunks';
+  return {
+    component_app: `component_app@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          },
+        ],
+      },
+    ];
+  },
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'host',
+        remotes: remotes(isServer),
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {},
+        extraOptions: {
+          debug: false, // `false` by default
+          exposePages: false, // `false` by default
+        },
+        shared: {},
+      })
+    );
+
+    return config;
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.thecocktaildb.com',
+        pathname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
